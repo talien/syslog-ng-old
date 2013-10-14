71bedc637e124c0e6a484b635c689283270872ca
Tusa Viktor <tusavik@gmail.com>
2013-10-15 01:02:41 +0200

    luaconfig: Reworked lua config for Parser based on previous refactor

--- lib/lua-parser.h
+++ lib/lua-parser.h
@@ -2,5 +2,7 @@
 #define _LUA_PARSER_H
 #include "luaconfig.h"
 
+#define LUA_PARSER_TYPE "SyslogNG.Parser"
+
 void cfg_lua_register_parser(lua_State *state);
 #endif
--- modules/dbparser/dbparser-lua.c
+++ modules/dbparser/dbparser-lua.c
@@ -1,13 +1,14 @@
 #include "dbparser-lua.h"
 #include "dbparser.h"
 #include "luaconfig.h"
+#include "lua-parser.h"
 
 int dbparser_lua_new(lua_State* state)
 {
     const char* name = g_strdup(lua_tostring(state, 1));
     LogParser* parser = log_db_parser_new();
     log_db_parser_set_db_file(parser, name);
-    lua_pushlightuserdata(state, parser);
+    lua_create_userdata_from_pointer(state, parser, LUA_PARSER_TYPE);
     return 1;
 }
 
@@ -15,5 +16,5 @@ void dbparser_register_lua_config(GlobalConfig* cfg)
 {
     pattern_db_global_init();
     lua_State* state = cfg->lua_cfg_state;
-    lua_register(state, "DBParser", dbparser_lua_new);
+    lua_register_driver(state, "DBParser", LUA_PARSER_TYPE, dbparser_lua_new);
 }
